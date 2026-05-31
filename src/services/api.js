731ecd4c7a925712fb4bed88ff03c75a1cadaec2
Lekk07 <148355584+Lekk07@users.noreply.github.com>
2026-05-31 10:08:04 +0530

    Update API base URL to use environment variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,18 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL
+    ? `${import.meta.env.VITE_API_URL}/api`
+    : '/api',
   headers: { 'Content-Type': 'application/json' },
 })
 
-// Attach token on every request if present
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token')
   if (token) config.headers.Authorization = `Bearer ${token}`
   return config
 })
 
-// On 401, clear token and redirect to login
 api.interceptors.response.use(
   res => res,
   err => {
@@ -23,5 +23,3 @@ api.interceptors.response.use(
     return Promise.reject(err)
   }
 )
-
-export default api
